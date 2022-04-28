48d01250284a46953aa9eae21b77f7c388153a63
mighty <kosuke20704@gmail.com>
2022-04-28 20:11:53 +0900

    lesson02

---
+++ master/lesson02/B1.java
@@ -0,0 +1,52 @@
+package lesson02;
+
+import GImage.GImage;
+
+public class B1 {
+	public static void main(String[] args)
+	{
+		 String fileName = "Sailboat0.bmp";
+	     GImage img= new GImage(fileName);
+	     GImage img0= new GImage(fileName);
+
+	     /**
+	      * Get the width of the image-file.
+	      */
+	     int width = img.getWidth();
+	     System.out.println("width="+width);
+
+	     /**
+	      * Get the height of the image-file.
+	      */
+	     int height = img.getHeight();
+	     System.out.println("height="+height);
+
+	     for(int i = 0; i < height; i++) {
+	    	 for(int l = 0; l < width; l++) {
+	    		 img.pixel[i][l]=img0.pixel[height-1-i][l];
+	    	 }
+	     }
+
+
+	     /**
+	      *  Set the file name to "My_ImageB1".
+	      */
+	     String fileName01 = "My_ImageB1";
+
+	     /**
+	      *  Set the file type to "bmp".
+	      */
+	     String fileType01 = "bmp";
+
+	     /**
+	      * Output the image-file "My_Image01.bmp".
+	      */
+	     img.output(fileName01,fileType01);
+
+	     /**
+	      * Set the file name to "My_Image01.bmp".
+	      */
+	     fileName01 +="." + fileType01;
+	     System.out.println("Output file:"+fileName01);
+	}
+}
